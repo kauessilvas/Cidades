cf515695dc8946c97eb8932d73ac3cde6e1d2b75
kauessilvas <kauesantosds5@gmail.com>
2025-02-25 19:33:20 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿string[] cities = ["Santos", "São Vicente", "Guarujá","Praia Grande", "Mongaguá", "Itanhaém", "Peruíbe", "Iguape", "Ilha Comprida", "Cananéia", "Pariquera-Açu"];
+
+for (int i = 0; i < cities.Length; i++)
+{
+    Console.WriteLine(cities[i]);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Cidades
+Programa que exibe os nomes das cidades do litoral sul de São Paulo, um em cada linha.
